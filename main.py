2c4d6387fa1396a3982ebc7712159ac634e66dcd
Jacqueline Espinosa <jacquelineespinosa@imac.myfiosgateway.com>
2022-10-28 09:05:08 -0400

    added destination function

---
+++ main.py
@@ -0,0 +1,13 @@
+
+fav_locations_list = ['Las Vegas', 'Vancouver', 'Cancun', 'New Orleans']
+
+def my_destinations():
+    for fav_location in fav_locations_list:
+        print ("")
+        print(fav_locations_list[0])
+        print(fav_locations_list[1])
+        print(fav_locations_list[2])
+        print(fav_locations_list[3])
+        print("")
+        break
+my_destinations()
