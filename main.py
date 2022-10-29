77bcf7eb86d0372698440380c83ea3068a691294
Jacqueline Espinosa <jacquelineespinosa@imac.myfiosgateway.com>
2022-10-29 13:44:58 -0400

    add a random.choice sequence

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 
+import random
+
+
 location_lists = ['Las Vegas', 'Vancouver', 'Cancun', 'New Orleans ']
-restaurants = ['Hells kitchen', 'Papadilla','Sofrito', 'Amy Ruth ']
-transportation = ['bus', 'train', 'flight']
+restaurant_list = ['Hells kitchen', 'Papadilla','Sofrito', 'Amy Ruth ']
+transportation_list = ['Bus', 'Train', 'Flight', 'Road trip']
 
 
 def my_destinations():
@@ -10,7 +13,23 @@ def my_destinations():
         
 my_destinations()
 
+print("")
 
+def my_resturants():
+    for restaurant in restaurant_list:
+        print (restaurant)
+        
+my_resturants()
 
+print("")
 
+def my_transportation():
+    for transportation in transportation_list:
+        print (transportation)
+        
+my_transportation()
 
+a = (random.choice(location_lists))
+b = (random.choice(restaurant_list))
+c = (random.choice(transportation_list))
+print(a+" ",b+" ", c)
