8bdfcbeb22c775f1eff62e5f0f285bf7590fe2aa
Jacqueline Espinosa <jacquelineespinosa@imac.myfiosgateway.com>
2022-10-29 13:07:56 -0400

    added function for mydestinations

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 
-fav_locations_list = ['Las Vegas', 'Vancouver', 'Cancun', 'New Orleans']
+location_lists = ['Las Vegas', 'Vancouver', 'Cancun', 'New Orleans ']
+restaurants = ['Hells kitchen', 'Papadilla','Sofrito', 'Amy Ruth ']
+transportation = ['bus', 'train', 'flight']
+
 
 def my_destinations():
-    for fav_location in fav_locations_list:
-        print ("")
-        print(fav_locations_list[0])
-        print(fav_locations_list[1])
-        print(fav_locations_list[2])
-        print(fav_locations_list[3])
-        print("")
-        break
+    for location in location_lists:
+        print (location)
+        
 my_destinations()
+
+
+
+
+
