b9cf85bd58d5d9c5983e077498cffc31c26316dc
Jacqueline Espinosa <jacquelineespinosa@imac.myfiosgateway.com>
2022-10-30 14:57:48 -0400

    removal of comments not in use

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
-
-import random
-
+import threading
 
 location_lists = ['Las Vegas', 'Vancouver', 'Cancun', 'New Orleans ']
 restaurant_list = ['Hells kitchen', 'Papadilla','Sofrito', 'Amy Ruth ']
 transportation_list = ['Bus', 'Train', 'Flight', 'Road trip']
-
-
-def my_destinations():
-    for location in location_lists:
-        print (location)
-        
-my_destinations()
+entertainment_list = ['A Play', "A concert", "Dancing", "Sightseeing"]
 
 print("")
 
-def my_resturants():
-    for restaurant in restaurant_list:
-        print (restaurant)
-        
-my_resturants()
 
-print("")
-
-def my_transportation():
-    for transportation in transportation_list:
-        print (transportation)
-        
-my_transportation()
+import random
 
 a = (random.choice(location_lists))
 b = (random.choice(restaurant_list))
 c = (random.choice(transportation_list))
-print(a+" ",b+" ", c)
+d = (random.choice(entertainment_list))
+print(a+" ",b+" ", c+" ", d)
+
+
+
+
