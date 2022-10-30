e0262651f4899ee09614bcca450b983ba0ef1fb4
Jacqueline Espinosa <jacquelineespinosa@imac.myfiosgateway.com>
2022-10-30 15:39:35 -0400

    additional random choices placed

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-import threading
 
 location_lists = ['Las Vegas', 'Vancouver', 'Cancun', 'New Orleans ']
-restaurant_list = ['Hells kitchen', 'Papadilla','Sofrito', 'Amy Ruth ']
-transportation_list = ['Bus', 'Train', 'Flight', 'Road trip']
-entertainment_list = ['A Play', "A concert", "Dancing", "Sightseeing"]
+restaurant_list = ['Hells kitchen', 'Papadilla','Sofrito', 'Amy Ruths ']
+transportation_list = ['a bus', 'a train', 'a flight', 'on a road trip']
+entertainment_list = ['at a theatrical show!', "at a concert!", "dancing!", "sightseeing!"]
 
 print("")
+
+input("Want a spontanous day trip?! Type Yes ")
+
+print("")
+
+import random
+a = (random.choice(location_lists))
+
+
+import random
+b = (random.choice(restaurant_list))
+
+
+import random
+c = (random.choice(transportation_list))
+
+
+import random
+d = (random.choice(entertainment_list))
+
 
 
 import random
@@ -14,7 +33,16 @@ a = (random.choice(location_lists))
 b = (random.choice(restaurant_list))
 c = (random.choice(transportation_list))
 d = (random.choice(entertainment_list))
-print(a+" ",b+" ", c+" ", d)
+print("You have an exciting trip to " + a + ".") 
+print("Your special place to dine is " + b + ".")
+print("Your mode of transportation is " + c + ".")
+print("You will have such an exciting time " + d + ".")
+
+print("")
+
+
+
+
 
 
 
