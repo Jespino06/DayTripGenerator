629ce1f7f70d7f1b1bbe5fe9516ddb2b8b7f10a5
Jacqueline Espinosa <jacquelineespinosa@imac.myfiosgateway.com>
2022-11-01 15:30:05 -0400

    finalized tweaks to overall project

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 
 Location_list = ['Las Vegas!', 'Vancouver!', 'Cancun!', 'New Orleans! ']
 Restaurant_list = ['Hells kitchen!', 'Papadilla!','Sofrito!', 'Amy Ruths! ']
-Transportation_list = ['a bus!', 'a train!', 'a flight!', 'on a road trip!']
+Transportation_list = ['a bus!', 'a train!', 'a flight!', 'a road trip!']
 Entertainment_list = ['at a theatrical show!', "at a concert!", "dancing!", "sightseeing!"]
+
+print("")
+
+def greeting():
+    message = "Hi there, welcome to Day Trip Generator! Let's get started!!! Your chosen adventure is...drum roll:"
+    print(message)
+
+greeting()
 
 print("")
 
@@ -21,43 +29,64 @@ randomly_selected_transportation = randomizer_list(Transportation_list)
 randomly_selected_entertainment = randomizer_list(Entertainment_list)
 
 
-
-
 def destination_generator():
-    user_choice = input("Are you satisfied with the selected destionation? Y/N ")
-    if user_choice == "Y":
+    randomizer_list(Location_list)
+    randomly_selected_destination = randomizer_list(Location_list)
+    print(randomly_selected_destination)
+
+    randomizer_list(Restaurant_list)
+    randomly_selected_restaurant = randomizer_list(Restaurant_list)
+    print(randomly_selected_restaurant)
+
+    randomizer_list(Transportation_list)
+    randomly_selected_transportation = randomizer_list(Transportation_list)
+    print(randomly_selected_transportation)
+
+    randomizer_list(Entertainment_list)
+    randomly_selected_entertainment = randomizer_list(Entertainment_list)
+    print(randomly_selected_entertainment)
+
+
+destination_generator()
+
+
+
+
+
+def customer_satisfaction():
+    print()
+    user_choice = input("Are you satisfied with the selected destionation, type Yes or No, change location, restaurant, transportation, or entertainment ")
+    if user_choice == "Yes":
         randomly_selected_destination = randomizer_list(Location_list)
         return randomly_selected_destination
-    elif user_choice == "N":
-        (randomizer_list)(Location_list)
-        newly_selected_location = randomizer_list(Location_list)
-        print(newly_selected_location)
-    elif user_choice == "N":
+    elif user_choice == "No, change restaurant.":
         (randomizer_list)(Restaurant_list)
         newly_selected_restaurant = randomizer_list(Restaurant_list)
         print(newly_selected_restaurant)
-    elif user_choice == "N":
+    elif user_choice == "No, change transportation.":
         (randomizer_list)(Transportation_list)
         newly_selected_transportation = randomizer_list(Transportation_list)
         print(newly_selected_transportation)
-    elif user_choice == "N":
+    elif user_choice == "No, change entertainment.":
         (randomizer_list)(Entertainment_list)
         newly_selected_entertainment = randomizer_list(Entertainment_list)
         print(newly_selected_entertainment)
+    
 
-destination_generator()
-print(randomly_selected_destination)
+customer_satisfaction()
 
 
+print("")
 
 def display_trip():
     result = (randomly_selected_destination + randomly_selected_restaurant + randomly_selected_transportation + randomly_selected_entertainment)
     return result
 
+
 display_trip()
-print(randomly_selected_destination)
-print(randomly_selected_restaurant)
-print(randomly_selected_transportation)
-print(randomly_selected_entertainment)
+print("Have a great time in " + randomly_selected_destination)
+print("Enjoy a delicious meal at " + randomly_selected_restaurant)
+print("Safe travels on " + randomly_selected_transportation)
+print("Have tons of fun " + randomly_selected_entertainment)
 
 print("")
